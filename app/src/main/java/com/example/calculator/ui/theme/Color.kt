6dcde64563b6317083fef39129b89621a25b146e
ChristielBel <kristina04bel@gmail.com>
2025-09-30 22:09:17 +0300

    refactor: add light and dark theme colors

--- app/src/main/java/com/example/calculator/ui/theme/Color.kt
+++ app/src/main/java/com/example/calculator/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.example.calculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightBackground = Color(0xFFF5F5F5)
+val LightResultBackground = Color(0xFFE0E0E0)
+val LightButton = Color(0xFFD6D6D6)
+val LightOperatorButton = Color(0xFFB0BEC5)
+val LightButtonText = Color(0xFF212121)
+val LightText = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkBackground = Color(0xFF121212)
+val DarkResultBackground = Color(0xFF1E1E1E)
+val DarkButton = Color(0xFF2C2C2C)
+val DarkOperatorButton = Color(0xFF37474F)
+val DarkButtonText = Color(0xFFFFFFFF)
+val DarkText = Color(0xFFFFFFFF)
