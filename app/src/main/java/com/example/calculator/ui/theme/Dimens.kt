ded9bb31ad0895ce7cb6247d5084a1a3d5ddff60
ChristielBel <kristina04bel@gmail.com>
2025-10-01 14:06:36 +0300

    refactor: increase font sizes

--- app/src/main/java/com/example/calculator/ui/theme/Dimens.kt
+++ app/src/main/java/com/example/calculator/ui/theme/Dimens.kt
@@ -6,5 +6,6 @@ import androidx.compose.ui.unit.sp
 val CalculatorPadding = 8.dp
 val CalculatorButtonSpacing = 8.dp
 val CalculatorButtonFontSize = 28.sp
-val CalculatorExpressionFontSize = 36.sp
-val CalculatorResultFontSize = 28.sp
+
+val CalculatorExpressionFontSize = 48.sp
+val CalculatorResultFontSize = 36.sp
